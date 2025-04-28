3b2f66f81a917a334d6226be7f012c1a9e4c1117
gudo1234 <izumilitee@gmail.com>
2025-04-28 07:46:19 -0600

    Update hola.js

--- plugins/hola.js
+++ plugins/hola.js
@@ -5,33 +5,6 @@ const fetch = require("node-fetch");
 const handler = async (msg, { conn }) => {
   const chatId = msg.key.remoteJid;
   
-  global.icono = [ 
-    'https://files.catbox.moe/ztexr8.jpg',
-    'https://files.catbox.moe/fd7x3t.jpg',
-    'https://files.catbox.moe/nsfx7f.jpg',
-    'https://files.catbox.moe/p3wdxz.jpg',
-    'https://files.catbox.moe/cbagtg.jpg',
-    'https://files.catbox.moe/ojqdd0.jpg',
-    'https://files.catbox.moe/9tkqgt.jpg',
-    'https://files.catbox.moe/3s7htp.jpg',
-    'https://files.catbox.moe/kkcj69.jpg',
-    'https://files.catbox.moe/mkjnzl.jpg',
-    'https://files.catbox.moe/zxwp9c.jpg',
-    'https://files.catbox.moe/p3fssk.jpg',
-    'https://files.catbox.moe/u5bspe.jpg',
-    'https://files.catbox.moe/wf4bb1.jpg',
-    'https://files.catbox.moe/f28poz.jpg',
-    'https://files.catbox.moe/dpx2s1.jpg',
-    'https://files.catbox.moe/wg1vbo.jpg',
-    'https://files.catbox.moe/grk81s.jpg'
-  ];
-  
-  // Elegimos una URL aleatoria
-  const randomIcon = icono[Math.floor(Math.random() * icono.length)];
-  
-  // Descargamos esa imagen
-  const thumbnail = await (await fetch(randomIcon)).buffer();
-  
   await conn.sendMessage(chatId, {
     text: 'test', 
     contextInfo: {
@@ -49,16 +22,13 @@ const handler = async (msg, { conn }) => {
         title: 'hola',
         body: 'hola mosha',
         thumbnailUrl: 'https://www.instagram.com/edar504__', // Aquí ahora sí una imagen válida
-        thumbnail: thumbnail,
+        thumbnail: imageBuffer,
         sourceUrl: 'https://www.instagram.com/edar504__' // Esto está bien aunque no sea imagen directa
       }
     }
   }, { quoted: msg });
 
-const imageBuffer = await global.getRandomIcon();
-if (imageBuffer) {
     await conn.sendMessage(chatId, { image: imageBuffer, caption: '🪐' });
-}
 
 };
 
