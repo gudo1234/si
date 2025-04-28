0697f2179abd4325fc149234c63b1126b38d7f9e
gudo1234 <izumilitee@gmail.com>
2025-04-27 22:51:09 -0600

    Update hola.js

--- plugins/hola.js
+++ plugins/hola.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const handler = async (msg, { conn, sock }) => {
   const chatId = msg.key.remoteJid;
   const thumbnail = await (await fetch(`https://files.catbox.moe/ztexr8.jpg`)).buffer();
-  sock.sendMessage2(chatId, {
+  sock.sendMessage(chatId, {
         text: 'test', 
         contextInfo: {
             mentionedJid: [],
