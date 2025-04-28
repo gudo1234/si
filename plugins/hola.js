fbe296b712988197715a60647258ed33ae778d25
gudo1234 <izumilitee@gmail.com>
2025-04-27 22:15:23 -0600

    Create hola.js

---
+++ plugins/hola.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const handler = async (msg, { conn }) => {
+  const chatId = msg.key.remoteJid;
+  const thumbnail = await (await fetch(icono)).buffer();
+  conn.sendMessage(chatId, {
+        text: wm, 
+        contextInfo: {
+            mentionedJid: [],
+            groupMentions: [],
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: ch,
+                newsletterName: wm,
+                serverMessageId: 0
+            },
+            businessMessageForwardInfo: { businessOwnerJid: '50492280729@s.whatsapp.net' },
+            forwardingScore: false,
+            externalAdReply: {
+                title: 'hola',
+                body: 'IzuBot te da la bienvenida',
+                thumbnailUrl: redes,
+                thumbnail,
+                sourceUrl: redes
+            }
+        }
+    }, { quoted: m });
+}
+handler.command = ['hola'];
+handler.reaction = '🔄';
+
+module.exports = handler;
