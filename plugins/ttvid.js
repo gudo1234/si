7a18eb31f963952957fdfd1069b0236e23855d6c
gudo1234 <izumilitee@gmail.com>
2025-05-03 12:58:40 -0600

    Update ttvid.js

--- plugins/ttvid.js
+++ plugins/ttvid.js
@@ -1,38 +1,45 @@
-const Starlights from '@StarlightsTeam/Scraper'
-//const @StarlightsTeam/Scraper = requiere('@StarlightsTeam/Scraper')
+const Starlights = require('@StarlightsTeam/Scraper');
+
 const handler = async (msg, { conn, text, usedPrefix }) => {
   if (!text) {
     return await conn.sendMessage(msg.key.remoteJid, {
-      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix}ttvid* La Vaca Lola`
+      text: `✖️ Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix}ttvid* La Vaca Lola`
     }, { quoted: msg });
   }
+
   try {
-    let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text)
-      let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
-          txt += `    ✩  *Título* : ${title}\n`
-          txt += `    ✩  *Autor* : ${author}\n`
-          txt += `    ✩  *Duración* : ${duration} segundos\n`
-          txt += `    ✩  *Vistas* : ${views}\n`
-          txt += `    ✩  *Likes* : ${likes}\n`
-          txt += `    ✩  *Comentarios* : ${comments_count}\n`
-          txt += `    ✩  *Compartidos* : ${share_count}\n`
-          txt += `    ✩  *Publicado* : ${published}\n`
-          txt += `    ✩  *Descargas* : ${download_count}\n\n`
-          txt += `> ${wm}`
+    let {
+      title, author, duration, views, likes,
+      comments_count, share_count, download_count,
+      published, dl_url
+    } = await Starlights.tiktokvid(text);
 
-      //await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m, null, rcanal)
+    let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n';
+    txt += `    ✩  *Título* : ${title}\n`;
+    txt += `    ✩  *Autor* : ${author}\n`;
+    txt += `    ✩  *Duración* : ${duration} segundos\n`;
+    txt += `    ✩  *Vistas* : ${views}\n`;
+    txt += `    ✩  *Likes* : ${likes}\n`;
+    txt += `    ✩  *Comentarios* : ${comments_count}\n`;
+    txt += `    ✩  *Compartidos* : ${share_count}\n`;
+    txt += `    ✩  *Publicado* : ${published}\n`;
+    txt += `    ✩  *Descargas* : ${download_count}\n\n`;
+    txt += `> ${wm}`;
 
-await conn.sendMessage(msg.key.remoteJid, {
-      video: dl_url,
+    await conn.sendMessage(msg.key.remoteJid, {
+      video: { url: dl_url },
       caption: txt,
-      mimetype: 'thumbnail.mp4',
-      fileName: txt)
+      mimetype: 'video/mp4',
+      fileName: `${title}.mp4`
     }, { quoted: msg });
 
-  } catch {
-    await m.react('✖️')
+  } catch (err) {
+    console.error(err);
+    await conn.sendMessage(msg.key.remoteJid, {
+      text: '✖️ Ocurrió un error al procesar el video. Asegúrate de que el enlace sea válido.'
+    }, { quoted: msg });
   }
-}
+};
 
-handler.command = ['tiktokvid','tiktoksearch','tiktokdl','ttvid']
+handler.command = ['tiktokvid','tiktoksearch','tiktokdl','ttvid'];
 module.exports = handler;
