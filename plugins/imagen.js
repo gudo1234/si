dfed46bcd94860703c5c7c0d91b14da83f9e0247
gudo1234 <izumilitee@gmail.com>
2025-05-03 21:12:34 -0600

    Create imagen.js

---
+++ plugins/imagen.js
@@ -0,0 +1,27 @@
+//import {googleImage} from '@bochilteam/scraper';
+const googleImage = require("@bochilteam/scraper")
+const handler = async (msg, { conn, text, usedPrefix, args }) => {
+  if (!text) {
+    return await conn.sendMessage2(msg.key.remoteJid, {
+      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix}ttimg* https://vm.tiktok.com/ZMBwnbFe7/`
+    }, msg );
+  }
+    await conn.sendMessage(msg.key.remoteJid, {
+            react: { text: "🕒", key: msg.key} 
+        });
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+await conn.sendMessage(
+  msg.key.remoteJid,
+  {
+    image: { url: await res.getRandom() },
+    caption: null,
+    fileName: 'image.jpg'
+  },
+  { quoted: msg }
+)
+};
+
+handler.command = ['image','imagen'];
+module.exports = handler;
