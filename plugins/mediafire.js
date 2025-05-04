e9d00be2fcd79ae51f5e04227a2c79b14428c246
gudo1234 <izumilitee@gmail.com>
2025-05-03 22:36:20 -0600

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -11,15 +11,12 @@ await conn.sendMessage(msg.key.remoteJid, {
         let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
   let gyh = await ouh.json() 
         //await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*Nombre:*  ${gyh.data[0].nama}\n*Peso:* ${gyh.data[0].size}*\n*Type:* ${gyh.data[0].mime}`, m, null, rcanal)
-         await conn.sendMessage2(
-  msg.key.remoteJid,
-  {
-    image: { url: gyh.data[0].link },
-    caption: `*Nombre:*  ${gyh.data[0].nama}\n*Peso:* ${gyh.data[0].size}*\n*Type:* ${gyh.data[0].mime}`,
-    fileName: `${gyh.data[0].nama}`
-  },
-  msg 
-)
+         await conn.sendMessage2(m.chat, {
+  document: { url: gyh.data[0].link },
+  fileName: gyh.data[0].nama,
+  mimetype: gyh.data[0].mime,
+  caption: `*Nombre:* ${gyh.data[0].nama}\n*Peso:* ${gyh.data[0].size}\n*Type:* ${gyh.data[0].mime}`
+}, msg );
         await conn.sendMessage(msg.key.remoteJid, {
             react: { text: "✅", key: msg.key} 
         });
