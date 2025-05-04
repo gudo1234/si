ee4456d8f2d7a4917b0af8c1e9fe30674acaa6ac
gudo1234 <izumilitee@gmail.com>
2025-05-04 02:51:36 -0600

    Update xnxxdl.js

--- plugins/xnxxdl.js
+++ plugins/xnxxdl.js
@@ -1,7 +1,7 @@
 const Starlights = require("@StarlightsTeam/Scraper");
 
 const handler = async (msg, { conn, text, usedPrefix, command, args }) => {
-  if (!args) {
+  if (!text) {
     return await conn.sendMessage2(msg.key.remoteJid, {
       text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* https://www.xnxx.es/video-1331hhfa/rubia_de_tetas_grandes_es_golpeada_y_un_bocado_de_semen`
     }, msg );
