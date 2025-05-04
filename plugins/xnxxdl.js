f544666792646afb046125ef6ff8ae8ce8698389
gudo1234 <izumilitee@gmail.com>
2025-05-04 02:26:22 -0600

    Update xnxxdl.js

--- plugins/xnxxdl.js
+++ plugins/xnxxdl.js
@@ -15,7 +15,7 @@ try {
 let { title, dl_url } = await Starlights.xnxxdl(args[0])
 //await conn.sendFile(m.chat, dl_url, title + '.mp4', *» Título* : ${title}, m, false, { asDocument: user.useDocument })
 
-await sock.sendMessage(msg.key.remoteJid, {
+await conn.sendMessage(msg.key.remoteJid, {
 video: dl_url,
 mimetype: 'video/mp4',
 fileName: `${title}.mp4`,
