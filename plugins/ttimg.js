bbd7296371bfe174cd7048bc5437f19bceaa4498
gudo1234 <izumilitee@gmail.com>
2025-05-03 19:36:59 -0600

    Update ttimg.js

--- plugins/ttimg.js
+++ plugins/ttimg.js
@@ -21,14 +21,14 @@ let txt = '`乂  T I K T O K - I M G`\n\n'
     txt += `\t\t*» Descargas* : ${downloads}\n`
        
 for (let i = 0; i < dl_urls.length; i++) {
-await conn.sendMessage(msg.key.remoteJid, dl_urls[i].dl_url, `tiktokimg${i + 1}.jpg`, txt, msg)
   await conn.sendMessage(
   msg.key.remoteJid,
   {
-    image: { url: dl_urls[i].dl_url }, 
-    caption: txt
+    image: { url: dl_urls[i].dl_url },
+    caption: txt,
+    fileName: `tiktokimg${i + 1}.jpg`
   },
-  msg 
+  msg
 )
 
 await conn.sendMessage(msg.key.remoteJid, {
