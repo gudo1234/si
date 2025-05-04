9cfbb17382a9dc8229e8a4d347ef06163f677468
gudo1234 <izumilitee@gmail.com>
2025-05-03 23:32:43 -0600

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -1,42 +1,21 @@
 const fetch = require("node-fetch");
 
-const handler = async (msg, { conn, text, usedPrefix, command }) => {
+const handler = async (msg, { conn, text, usedPrefix }) => {
   if (!text) {
     return await conn.sendMessage2(msg.key.remoteJid, {
-      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* link`
+      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix}mediafire* link`
     }, msg );
   }
-
-  /*await conn.sendMessage(msg.key.remoteJid, {
-    react: { text: "🕒", key: m.key }
-  });*/
-
-  try {
-    let res = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`);
-    if (!res.ok) throw 'No se pudo obtener respuesta de la API.';
-
-    let data = await res.json();
-
-    if (!data || !data.url) throw 'No se pudo obtener el archivo. Asegúrate de que el enlace sea correcto.';
-
-    let { url, filename, size, mime } = data;
-
-    await conn.sendMessage(msg.key.remoteJid, {
-      document: { url },
-      fileName: filename,
-      mimetype: mime || 'application/octet-stream',
-      caption: `*Nombre:* ${filename}\n*Tamaño:* ${size}\n*Enlace directo:* ${url}`
+        let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
+  let gyh = await ouh.json() 
+        //await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*Nombre:*  ${gyh.data[0].nama}\n*Peso:* ${gyh.data[0].size}*\n*Type:* ${gyh.data[0].mime}`, m, null, rcanal)       
+ await conn.sendMessage(msg.key.remoteJid, {
+      document: { url: gyh.data[0].link },
+      fileName: `${gyh.data[0].nama}`,
+      //mimetype: mime || 'application/octet-stream',
+      caption: `*Nombre:*  ${gyh.data[0].nama}\n*Peso:* ${gyh.data[0].size}*\n*Type:* ${gyh.data[0].mime}`
     }, { quoted: msg });
-
-  } catch (e) {
-    console.error(e);
-    //m.reply('Ocurrió un error al procesar el enlace. Asegúrate de que sea válido y vuelve a intentarlo.');
-  }
-
-  /*await conn.sendMessage(msg.key.remoteJid, {
-    react: { text: "✅", key: m.key }
-  });*/
-};
+}
 
 handler.command = ['mf', 'mediafire'];
 module.exports = handler;
