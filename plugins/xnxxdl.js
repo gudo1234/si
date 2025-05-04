3ff83e88ad534a2d88928376a346b69b9ad282d3
gudo1234 <izumilitee@gmail.com>
2025-05-04 02:21:11 -0600

    Update xnxxdl.js

--- plugins/xnxxdl.js
+++ plugins/xnxxdl.js
@@ -3,28 +3,40 @@ const Starlights = require("@StarlightsTeam/Scraper");
 const handler = async (msg, { conn, text, usedPrefix, command }) => {
   if (!text) {
     return await conn.sendMessage2(msg.key.remoteJid, {
-      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* https://www.xnxx.es/video-1331hhfa/rubia_de_tetas_grandes_es_golpeada_y_un_bocado_de_semen`
+      text: `❗ Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* https://www.xnxx.es/video-1331hhfa/rubia_de_tetas_grandes_es_golpeada_y_un_bocado_de_semen`
     }, msg);
   }
-await conn.sendMessage(msg.key.remoteJid, {
-            react: { text: "🕒", key: msg.key} 
-        });
-try {
-let { title, dl_url } = await Starlights.xnxxdl(args[0])
-//await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}`, m, false, { asDocument: user.useDocument })
 
-await sock.sendMessage(msg.key.remoteJid, {
-            video: dl_url,
-            mimetype: 'video/mp4',
-            fileName: `${title}.mp4`,
-            caption: `*» Título* : ${title}`
-        }, { quoted: msg });
+  await conn.sendMessage(msg.key.remoteJid, {
+    react: { text: "🕒", key: msg.key }
+  });
 
-await conn.sendMessage(msg.key.remoteJid, {
-            react: { text: "✅", key: msg.key} 
-        });
-} catch {
-}}
+  try {
+    let { title, dl_url } = await Starlights.xnxxdl(text);
 
-handler.command = ['xnxxdl']
+    await conn.sendMessage(msg.key.remoteJid, {
+      video: dl_url,
+      mimetype: 'video/mp4',
+      fileName: `${title}.mp4`,
+      caption: `*» Título* : ${title}`
+    }, { quoted: msg });
+
+    await conn.sendMessage(msg.key.remoteJid, {
+      react: { text: "✅", key: msg.key }
+    });
+
+  } catch (err) {
+    console.error("Error al descargar el video:", err); // Mostrar el error en consola
+
+    await conn.sendMessage(msg.key.remoteJid, {
+      text: "❌ Ocurrió un error al intentar descargar el video. Asegúrate de que el enlace sea válido y vuelve a intentarlo."
+    }, msg);
+
+    await conn.sendMessage(msg.key.remoteJid, {
+      react: { text: "❌", key: msg.key }
+    });
+  }
+};
+
+handler.command = ['xnxxdl'];
 module.exports = handler;
