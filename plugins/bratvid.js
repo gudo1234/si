9a92122588bf45683ccd87b5773cdf73c33a0318
gudo1234 <izumilitee@gmail.com>
2025-05-04 18:27:34 -0600

    Update bratvid.js

--- plugins/bratvid.js
+++ plugins/bratvid.js
@@ -1,16 +1,16 @@
 const fetch = require("node-fetch");
 
-const handler = async (msg, { conn, text, usedPrefix, command, args }) => {
+const handler = async (msg, { conn, text, usedPrefix, command }) => {
   if (!text) {
     return await conn.sendMessage2(msg.key.remoteJid, {
-      text: `${e} Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* hola mundo`
+      text: `❌ Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* hola mundo`
     }, msg);
   }
-await conn.sendMessage(msg.key.remoteJid, {
-            react: { text: "🕒", key: msg.key} 
-        });
 
-  const text = text.join(' ');
+  await conn.sendMessage(msg.key.remoteJid, {
+    react: { text: "🕒", key: msg.key }
+  });
+
   const apiUrl = `https://api.nekorinn.my.id/maker/bratvid?text=${encodeURIComponent(text)}`;
 
   try {
@@ -19,21 +19,23 @@ await conn.sendMessage(msg.key.remoteJid, {
 
     const buffer = await response.buffer();
 
-await conn.sendMessage2(msg.key.remoteJid, {
-      video: { url: buffer },
-      mimetype: 'bratvid.mp4',
-      //fileName: `${title}.mp4`,
-      caption: `${e} *Video generado para:* _${text}_`
-    }, msg );
+    await conn.sendMessage2(msg.key.remoteJid, {
+      video: buffer,
+      mimetype: 'video/mp4',
+      caption: `✅ *Video generado para:* _${text}_`
+    }, msg);
+
     await conn.sendMessage(msg.key.remoteJid, {
-            react: { text: "✅", key: msg.key} 
-        });
+      react: { text: "✅", key: msg.key }
+    });
+
   } catch (err) {
     console.error('Error al descargar el video:', err);
     await conn.sendMessage2(msg.key.remoteJid, {
-      text: `${e} Ocurrió un error al intentar descargar el video, pruebe más tarde`
-    }, msg );
-  }}
+      text: `❌ Ocurrió un error al intentar descargar el video, pruebe más tarde`
+    }, msg);
+  }
+};
 
 handler.command = ['bratvid', 'vidbrat'];
 module.exports = handler;
