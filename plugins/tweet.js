9e5a0845a0bed7c53754ad3b0d7e725c9ab8eecb
gudo1234 <izumilitee@gmail.com>
2025-05-05 16:53:52 -0600

    Update tweet.js

--- plugins/tweet.js
+++ plugins/tweet.js
@@ -12,13 +12,12 @@ const handler = async (msg, { conn, text, usedPrefix, command }) => {
 
     const sender = msg.key.participant || msg.key.remoteJid;
     const avatar = await conn.profilePictureUrl(sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
-    const displayName = await conn.getName(sender);
     const username = sender.split('@')[0];
     const replies = '69';
     const retweets = '69';
     const theme = 'dark';
 
-    const url = `https://some-random-api.com/canvas/misc/tweet?displayname=${encodeURIComponent(displayName)}&username=${encodeURIComponent(username)}&avatar=${encodeURIComponent(avatar)}&comment=${encodeURIComponent(text)}&replies=${encodeURIComponent(replies)}&retweets=${encodeURIComponent(retweets)}&theme=${encodeURIComponent(theme)}`;
+    const url = `https://some-random-api.com/canvas/misc/tweet?displayname=${encodeURIComponent(`${msg.pushName}`)}&username=${encodeURIComponent(username)}&avatar=${encodeURIComponent(avatar)}&comment=${encodeURIComponent(text)}&replies=${encodeURIComponent(replies)}&retweets=${encodeURIComponent(retweets)}&theme=${encodeURIComponent(theme)}`;
 
     await conn.sendMessage2(msg.key.remoteJid, {
       image: { url: url },
