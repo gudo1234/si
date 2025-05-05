b250eba506bda228540d9b79b5a8df8ccf8a32c9
gudo1234 <izumilitee@gmail.com>
2025-05-04 18:36:00 -0600

    Create bratvid.js

---
+++ plugins/bratvid.js
@@ -0,0 +1,44 @@
+const fetch = require("node-fetch");
+
+const handler = async (msg, { conn, text, usedPrefix, command, args }) => {
+  if (!text) {
+    return await conn.sendMessage2(msg.key.remoteJid, {
+      text: `❌ Usa el comando correctamente:\n\n📌 Ejemplo: *${usedPrefix + command}* hola mundo`
+    }, msg);
+  }
+
+  await conn.sendMessage(msg.key.remoteJid, {
+    react: { text: "🕒", key: msg.key }
+  });
+
+  const query = encodeURIComponent(text);
+  const apiUrl = `https://api.nekorinn.my.id/maker/bratvid?text=${query}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error(`Error al obtener respuesta: ${response.statusText}`);
+
+    const json = await response.json();
+    const { data } = json;
+
+    if (!data) throw new Error("Respuesta inválida de la API");
+
+    await conn.sendMessage2(msg.key.remoteJid, {
+      video: { url: data },
+      mimetype: 'video/mp4',
+      caption: `✅ *Video generado para:* _${text}_`
+    }, msg);
+
+    await conn.sendMessage(msg.key.remoteJid, {
+      react: { text: "✅", key: msg.key }
+    });
+  } catch (err) {
+    console.error('Error al descargar el video:', err);
+    await conn.sendMessage2(msg.key.remoteJid, {
+      text: `⚠️ Ocurrió un error al intentar descargar el video, pruebe más tarde`
+    }, msg);
+  }
+};
+
+handler.command = ['bratvid', 'vidbrat'];
+module.exports = handler;
