13db62cc6b70e2bb9f92f9e81016f13318437c64
gudo1234 <izumilitee@gmail.com>
2025-05-05 09:58:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -24,6 +24,8 @@ global.e = '🪐'
 global.wm = '🤖⃧►iʑυвöτ◃2.0▹'
 global.textbot = 'sɪᴍᴘʟᴇ ʙᴏᴛ - ᴡʜᴀᴛsᴀᴘᴘ 2019-2025'
 global.channel = '120363285614743024@newsletter'
+global.packname = '🤖⃧►iʑυвöτ◃2.0▹'
+global.author = '―͟͞🍁͓̽𝆥⟅᮫ₑᵤ⟆𝇂𝇃𝇄͓⳻֟፝͜⳺𝇄𝇃𝇂͓'
 
 // ✅ Lista de prefijos permitidos
 global.allowedPrefixes = [
