e4e58009ddcbef01811403fc38f6702b01e796e0
gudo1234 <izumilitee@gmail.com>
2025-05-06 06:13:56 -0600

    Update que.js

--- plugins/que.js
+++ plugins/que.js
@@ -1,7 +1,20 @@
-const handler = async (msg, { conn }) => {
+/*const handler = async (msg, { conn }) => {
 await conn.sendMessage2(msg.key.remoteJid, {
       text: `zo🧀`
     }, msg);
   }
 handler.command = ['que'];
+module.exports = handler;*/
+
+const handler = async (msg, { conn }) => {
+  const texto = msg.message?.conversation || msg.message?.extendedTextMessage?.text;
+  if (!texto) return;
+
+  if (texto.toLowerCase() === 'que') {
+    await conn.sendMessage2(msg.key.remoteJid, {
+      text: 'zo🧀'
+    }, msg);
+  }
+};
+
 module.exports = handler;
