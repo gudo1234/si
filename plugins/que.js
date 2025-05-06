8e36fa2a8bf87ce958c37047ec2434c64165e09d
gudo1234 <izumilitee@gmail.com>
2025-05-06 06:26:31 -0600

    Delete plugins/que.js

--- plugins/que.js
+++
@@ -1,9 +0,0 @@
-const handler = async (msg, { conn }) => {
-  await conn.sendMessage(msg.key.remoteJid, {
-    text: 'zo🧀'
-  }, { quoted: msg });
-};
-
-handler.customPrefix = false; // false si usas un prefijo como "." o "!"
-handler.command = /^zo$/i; // El comando será "zo", sin prefijo si customPrefix = true
-module.exports = handler;
