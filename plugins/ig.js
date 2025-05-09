91e1622cb6cedad51109c30010c5fe016d2571c8
gudo1234 <izumilitee@gmail.com>
2025-05-09 10:12:26 -0600

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -13,9 +13,9 @@ let { dl_url } = await Starlights.igdl(args[0])
 //await conn.sendFile(m.chat, dl_url, 'igdl.mp4', listo, m, null, rcanal)
 await conn.sendMessage2(msg.key.remoteJid, {
       video: { url: dl_url },
-      mimetype: 'video/mp4',
+      mimetype: 'igdl.mp4'
       //fileName: listo.mp4,
-      caption: listo
+      //caption: listo
     }, msg );
 await conn.sendMessage(msg.key.remoteJid, {
             react: { text: "✅", key: msg.key} 
